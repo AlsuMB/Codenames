5a5a5f1e80a82bef12d32798142b16155af84559
AlsuMB <65676823+AlsuMB@users.noreply.github.com>
2020-11-29 15:03:04 +0300

    update disign and add randomizer cards

---
+++ src/sample/controllers/MainWindowInGame.java
@@ -0,0 +1,121 @@
+package sample.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class MainWindowInGame {
+
+    @FXML
+    private Text word_1;
+
+    @FXML
+    private Text word_4;
+
+    @FXML
+    private Text word_3;
+
+    @FXML
+    private GridPane word_2;
+
+    @FXML
+    private Text word_5;
+
+    @FXML
+    private GridPane word_11;
+
+    @FXML
+    private Text word_6;
+
+    @FXML
+    private GridPane word_16;
+
+    @FXML
+    private Text remaining_words_blue;
+
+    @FXML
+    private GridPane word_21;
+
+    @FXML
+    private Text remaining_words_red;
+
+    @FXML
+    private Text count_black;
+
+    @FXML
+    private Text count_white;
+
+    @FXML
+    private Text port_and_host;
+
+    @FXML
+    private Text word_12;
+
+    @FXML
+    private Text word_7;
+
+    @FXML
+    private GridPane word_17;
+
+    @FXML
+    private Text word_22;
+
+    @FXML
+    private Text word_13;
+
+    @FXML
+    private GridPane word_8;
+
+    @FXML
+    private Text word_18;
+
+    @FXML
+    private GridPane word_23;
+
+    @FXML
+    private Text word_14;
+
+    @FXML
+    private Text word_9;
+
+    @FXML
+    private Text word_19;
+
+    @FXML
+    private GridPane word_24;
+
+    @FXML
+    private Text word_15;
+
+    @FXML
+    private GridPane word_10;
+
+    @FXML
+    private Text word_20;
+
+    @FXML
+    private Text word_25;
+
+    @FXML
+    private TextField input_prompt_blue;
+
+    @FXML
+    private GridPane gamers_in_blue;
+
+    @FXML
+    private Text gamers_in_red;
+
+    @FXML
+    private TextField input_prompt_red;
+
+    @FXML
+    private Text all_prompt_blue;
+
+    @FXML
+    private Text all_prompt_red;
+
+
+
+}
+
--- src/sample/helpers/RandomCards.java
+++ src/sample/helpers/RandomCards.java
@@ -1,4 +1,40 @@
 package sample.helpers;
 
+import sun.awt.windows.WPrinterJob;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class RandomCards {
+    public List give25Cards() {
+        WorkWithCSV workWithCSV = new WorkWithCSV();
+        List<String> allWords = workWithCSV.giveAllWords("src/sample/templates/words.csv");
+        List<Integer> numbers = randomNumbers(allWords.size());
+        ArrayList<String> res = new ArrayList<>();
+        for (int element : numbers) {
+            res.add(allWords.get(element));
+        }
+        return res;
+    }
+
+    public ArrayList<Integer> randomNumbers(int maxSize) {
+        int i = 0;
+        Set<Integer> setNumber = new HashSet<>();
+        while (i != 25) {
+            int random = (int) (Math.random() * (maxSize));
+
+            if (!setNumber.contains(random)) {
+                setNumber.add(random);
+                i++;
+            }
+        }
+        return new ArrayList<>(setNumber);
+    }
+
+    public static void main(String[] args) {
+        RandomCards randomCards = new RandomCards();
+        System.out.println(randomCards.give25Cards());
+    }
 }
