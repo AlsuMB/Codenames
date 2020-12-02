ce7d68fa3ffccba214ecd12971b34953bcfbad01
AlsuMB <65676823+AlsuMB@users.noreply.github.com>
2020-12-02 10:44:45 +0300

    add models, helpers and controller

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,29 +1,19 @@
-package sample.controllers;
+package sample;
 
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("allFXML/sample.fxml"));
-        primaryStage.setMaximized(true);
+        Parent root = FXMLLoader.load(getClass().getResource("allFXML/enter.fxml"));
+//        primaryStage.setMaximized(true);
         primaryStage.setTitle("Codenames");
-        primaryStage.setScene(new Scene(root, 1080, 720));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
---
+++ src/sample/models/CardsColor.java
@@ -0,0 +1,9 @@
+package sample.models;
+
+public enum CardsColor {
+    BLUE,
+    RED,
+    BLACK,
+    WHITE,
+    NOBODIES
+}
